ed68652d01d1f7f5901d6ffb829c2c0963554ceb
danielh-oliveira <danielh_oliveira@yahoo.com.br>
2022-01-18 22:24:36 -0300

    Criei o projeto e adicionei uma activity vazia

---
+++ app/src/main/java/br/com/daniel/danielviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package br.com.daniel.danielviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.daniel.danielviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
