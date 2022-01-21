68929b32a3ca097b53e2e3dfb0d98b3cf793762b
danielh-oliveira <danielh_oliveira@yahoo.com.br>
2022-01-20 22:40:40 -0300

    Criando layout de activity de resumo

---
+++ app/src/main/java/br/com/daniel/danielviagens/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,16 @@
+package br.com.daniel.danielviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.daniel.danielviagens.R;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pacote);
+    }
+}
